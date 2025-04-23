447d344f994c5f48f8b30f3f6f75f23d3fd554b5
jackireland2 <121182167+jackireland2@users.noreply.github.com>
2025-04-23 02:17:23 +0100

    lab10

--- common/maths.hpp
+++ common/maths.hpp
@@ -1,1 +1,38 @@
+#pragma once
 
+#include<iostream>
+#include<cmath>
+#include<glm/glm.hpp>
+#include<glm/gtx/io.hpp>
+
+
+
+// Quaternion class
+class Quaternion
+{
+public:
+	float w, x, y, z;
+
+	glm::mat4 matrix();
+
+
+	// Constructors
+	Quaternion();
+	Quaternion(const float w, const float x, const float y, const float z);
+	Quaternion(const float pitch, const float yaw);
+};
+//maths class
+class Maths {
+
+
+public:
+	//transformation matrices
+	static glm::mat4 translate(const glm::vec3& v);
+	static glm::mat4 scale(const glm::vec3& v);
+	static float radians(float angle);
+	static glm::mat4 rotate(const float& angle, glm::vec3 v);
+
+
+	static Quaternion SLERP(const Quaternion q1, const Quaternion q2, const float t);
+
+};
